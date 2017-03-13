261970fa90f1e03cad3c0ff09389296207c2ad9b
Nathan Ziarek <nziarek@directs.com>
2017-03-13 15:55:43 -0500

    retrieving team schedules

---
+++ retrieve-team-schedule.js
@@ -0,0 +1,18 @@
+var request = require('request'),
+    unique = require('array-unique');
+
+const teamId = 'norfolk-state-spartans',
+		teamScheduleUrl = `http://www.si.com/college-basketball/team/${teamId}/schedule`,
+      gameIdList = [];
+
+console.log(teamScheduleUrl);
+
+request(teamScheduleUrl, function (error, response, body) {
+  if(response && response.statusCode == 200) {
+    const recapLinks = body.match(/\/college-basketball\/game\/\d+/gim);
+    recapLinks.map(function(data){
+      gameIdList.push(data.match(/\d+/)[0]);
+    });
+    gameIdList = unique(gameIdList);
+  }
+});
